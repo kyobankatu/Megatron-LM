365cd3b3fdc521fb24daf86246fb5d2b7f9653ad
K Ota <>
2026-05-27 14:55:02 +0900

    change: edit batch file

--- run_rank.sh
+++ run_rank.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+set -e
+
+module load openmpi/5.0.10-gcc
+source /gs/bs/hp190122/ux07012/.venv/llm_lecture/bin/activate
+cd ~/develop/LLM_lecture/Megatron-LM
+
 export CUDA_DEVICE_MAX_CONNECTIONS=1
-export MASTER_ADDR=`head -n 1 $SGE_JOB_SPOOL_DIR/pe_hostfile | cut -d " " -f 1`
+export MASTER_ADDR=$(head -n 1 "$SGE_JOB_SPOOL_DIR/pe_hostfile" | cut -d " " -f 1)
 export MASTER_PORT=29508
 export PYTHONPATH=$PWD
 
